c107085839d5e044e013c7df07f578247eb964b3
thabiso <tha@gmail.com>
2025-10-30 21:58:21 +0200

    added code for object id

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,14 @@
 //CRUD operation
 
-import {MongoClient, MongoServerError} from "mongodb";
+import {MongoClient, MongoServerError, ObjectId} from "mongodb";
 
 const connectionUrl = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(connectionUrl);
 
 //const databaseName = "task-manager";
+const id = new ObjectId();
+console.log(id);
+console.log(id.getTimestamp());
 
 async function writeDB(userDocuments, databaseName, collectionName){
     
@@ -16,9 +19,12 @@ async function writeDB(userDocuments, databaseName, collectionName){
 
     //const insertResult = await collection.insertOne(  );
     try{
-        const insertResult = await collection.insertMany(userDocuments);
-        console.log("Insert Result: ", insertResult );
-
+        if(userDocuments.length != 0) {
+            const insertResult = await collection.insertMany(userDocuments);
+            console.log("Insert Result: ", insertResult );
+        }else{
+            console.log("No documents to insert");
+        }
     }catch(error){
         if(error instanceof MongoServerError){
             console.log("Mongo Server Error :",error);
@@ -30,21 +36,21 @@ async function writeDB(userDocuments, databaseName, collectionName){
 };
 
 
-// adding user documents/records to users collection/table
+// ====== adding user documents/records to users collection/table =======
 // writeDB([{name:"Freddy",Age:100},{name:"Stanford",Age:98}], "task-manager", "users" )
 // .then(console.log)
 // .catch(console.error)
 // .finally( () => client.close() );
 
 
-// adding task documents/records to tasks collection/table
-const toDoList = [
-    {description:"go to gym", done:true},
-    {description:"buy grocery after gym", done:false},
-    {description:"do laundry", done:true}
-];
+// ======= adding task documents/records to tasks collection/table =======
+// const toDoList = [
+//     {description:"go to gym", done:true},
+//     {description:"buy grocery after gym", done:false},
+//     {description:"do laundry", done:true}
+// ];
 
-writeDB(toDoList, "task-manager", "tasks" )
-.then(console.log)
-.catch(console.error)
-.finally( () => client.close() );
+// writeDB(toDoList, "task-manager", "tasks" )
+// .then(console.log)
+// .catch(console.error)
+// .finally( () => client.close() );
